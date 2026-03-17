6bd554441e657b0db9bd0ee05c7efa9b6aac79de
surya <surya2112singh@gmail.com>
2026-03-17 13:15:08 +0530

    final project

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import './App.css';
+import {useState} from 'react';
+
+function Square({value, onSquareClick}){
+  return (
+    <button
+    className="square" data-value={value} onClick={onSquareClick}>
+      {/* {value} */}
+    </button>
+  );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i=0; i<lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  const status = winner 
+    ? `Winner: ${winner}` 
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  function handleClick(i) {
+    if (squares[i] || winner) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function handleReset() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-header">
+        <h1>Welcome To Tic Tac Toe Game</h1>
+        <h2>Refresh Your childhood</h2>
+      </div>
+      
+      <div className="status" status-value = {status}>
+        {/* {status} */}
+        </div>
+      
+      <div className="board">
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+      
+      <button className="reset-btn" onClick={handleReset}>
+        New Game
+      </button>
+    </div>
+  );
+}
+
+
+export default App;
